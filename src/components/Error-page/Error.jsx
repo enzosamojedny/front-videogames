cff8742e1bd6cae420746e884b76d6e5d3fcaa5f
enzosamojedny <enzosamojedny@gmail.com>
2023-10-11 11:52:52 -0300

    last commit

--- src/components/Error-page/Error.jsx
+++ src/components/Error-page/Error.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './error.css';
-
+import { NavLink } from 'react-router-dom';
 function Error() {
     return (
         <div className="error-container">
             <div>
+                <NavLink to={'/home'}><button className="error-button">Go back</button></NavLink>
                 <h1 className="error-heading">404 Not Found</h1>
                 <img src="../../../404.png" alt="" className="error-image" />
             </div>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './footer.css'
+import { NavLink } from 'react-router-dom'
 function Footer() {
     return (
         <div className='footer-whole-wrapper'>
             <div className='footer-wrapper'>
                 <div className='content-wrapper'>
-                    <p style={{ color: 'white' }}>FOOTER</p>
+                    <NavLink to={'https://github.com/enzosamojedny'} target="_blank">
+                        <img className='img' src="https://github.com/fluidicon.png" alt="Git hub" />
+                    </NavLink>
+                    <p style={{ color: 'white' }}>Developed by Enzo Samojedny</p>
                 </div>
             </div>
         </div>
